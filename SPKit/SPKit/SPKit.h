2599585e966399380cf52f7f2502c94cca249e74
Bruno Pires <bruno@blastersystems.com>
2016-11-30 02:11:48 +0000

    initial commit

---
+++ SPKit/SPKit/SPKit.h
@@ -0,0 +1,19 @@
+//
+//  SPKit.h
+//  SPKit
+//
+//  Created by Bruno Pires on 30/11/16.
+//  Copyright © 2016 BlasterSystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPKit.
+FOUNDATION_EXPORT double SPKitVersionNumber;
+
+//! Project version string for SPKit.
+FOUNDATION_EXPORT const unsigned char SPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPKit/PublicHeader.h>
+
+
